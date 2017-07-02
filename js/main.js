0b58698dffd5f927141ab3c23f32b79064a5dbf2
Pedro Lima <pedrova@ifi.uio.no>
2017-07-03 00:01:45 +0200

    Add article dates

--- js/main.js
+++ js/main.js
@@ -15,21 +15,43 @@
             var images = [];
             var headings = [];
             var texts = [];
+            var articleDate = "";
+            var monthNames = ["jan", "feb", "mar", "apr", "mai", "jun", "jul", "aug", "sep", "okt", "nov", "des"];
+            var displayDate = "";
+            var displayDates = [];
 
             // get JSON contents
-            $.each(data.items, function (index, item) {
+            $.each(data.items, function(index, item) {
                 listTags.push("<li><div></div></li>");
                 images.push('<span class="image" style="background-image: url('+item.cover_image+')"></span>');
                 headings.push("<span><h3>"+item.contents.title+"</h3></span>");
                 texts.push("<span>"+item.contents.preamble+"</span>");
+                articleDate = new Date(item.meta.created);
+                displayDate = articleDate.getDate() + ' ' + monthNames[articleDate.getMonth()];
+                displayDates.push(displayDate);
             });
 
             // Add elements on the page
             $('.timeline').find("ul").append( listTags.join('') );
-            $("li > div").each(function(index){
+            $("li > div").each(function(index) {
                 $( this ).append(images[index]);
                 $( this ).append(headings[index]);
                 $( this ).append(texts[index]);
+            });
+
+            // Add dates in li::after pseudo-element
+            var currentDate = "";
+            var previousDate = "";
+            $(".timeline li").each(function(index) {
+                currentDate = displayDates[index];
+                if (index > 0) {
+                    previousDate = displayDates[index-1];
+                }
+                if (currentDate === previousDate) {
+                    $( this).addClass("samedate");
+                    return true;
+                }
+                $( this ).attr('data-content', displayDates[index]);
             });
         });
 
