94f4e82421cd17133938a85669f28633a3bd149f
Pedro Lima <pedrova@ifi.uio.no>
2017-07-03 19:12:58 +0200

    REFACTOR: Change the way we get and display the JSON data

--- js/main.js
+++ js/main.js
@@ -5,87 +5,63 @@
 
         // Get Articles
         // TODO: Add ajax on fail
-        $.ajax({
+        $.get({
             url: urlArticles,
-            type: "GET",
             dataType: "jsonp"
         }).done(function(data) {
-            console.log(data);
-            var listTags = [];
-            var images = [];
-            var headings = [];
-            var texts = [];
             var articleDate = "";
             var monthNames = ["jan", "feb", "mar", "apr", "mai", "jun", "jul", "aug", "sep", "okt", "nov", "des"];
             var displayDate = "";
-            var displayDates = [];
+            var previousDate = "";
 
-            // get JSON contents
+            // get JSON contents and add on the page
             $.each(data.items, function(index, item) {
-                listTags.push("<li><div></div></li>");
-                images.push('<span class="image" style="background-image: url('+item.cover_image+')"></span>');
-                headings.push("<span><h3>"+item.contents.title+"</h3></span>");
-                texts.push("<span>"+item.contents.preamble+"</span>");
+                $('.timeline').find("ul").append(
+                    '<li id="li_' + index + '"><div>' +
+                    '<span class="image" style="background-image: url(' + item.cover_image + ')"></span>' +
+                    '<span><h3>' + item.contents.title + '</h3></span>' +
+                    '<span>' + item.contents.preamble + '</span>' +
+                    '</div></li>'
+                );
+
                 articleDate = new Date(item.meta.created);
                 displayDate = articleDate.getDate() + ' ' + monthNames[articleDate.getMonth()];
-                displayDates.push(displayDate);
-            });
 
-            // Add elements on the page
-            $('.timeline').find("ul").append( listTags.join('') );
-            $("li > div").each(function(index) {
-                $( this ).append(images[index]);
-                $( this ).append(headings[index]);
-                $( this ).append(texts[index]);
-            });
-
-            // Add dates in li::after pseudo-element
-            var currentDate = "";
-            var previousDate = "";
-            $(".timeline li").each(function(index) {
-                currentDate = displayDates[index];
-                if (index > 0) {
-                    previousDate = displayDates[index-1];
-                }
-                if (currentDate === previousDate) {
-                    $( this).addClass("samedate");
+                // Add dates in li::after pseudo-element
+                if (displayDate === previousDate) {
+                    $("#li_" + index).addClass("samedate");
                     return true;
                 }
-                $( this ).attr('data-content', displayDates[index]);
+                $("#li_" + index).attr('data-content', displayDate);
+                previousDate = displayDate;
             });
         });
 
         // Get Channels
         // TODO: Add ajax on fail
-        $.ajax({
+        $.get({
             url: urlChannels,
-            type: "GET",
             dataType: "jsonp"
         }).done(function(data) {
-            console.log(data);
-            var listTags = [];
-            var images = [];
-
-            // get JSON contents
             var active = 'class="active"';
+
+            // get JSON contents and add on the page
             $.each(data.items, function (index, item) {
-                listTags.push('<li ' + active + ' style="background-image: url('+item.cover_image+')"></li>');
+                $('.navbar-header').find("ul").append(
+                    '<li id="navli_' + index + '" ' + active + ' style="background-image: url(' + item.cover_image + ')"></li>'
+                );
                 active = '';
-                images.push('<a class="link" href="#" data-toggle="pill">'+item.channel.name+'</a>');
+                $( "#navli_" + index).append(
+                    '<a class="link" href="#" data-toggle="pill">' + item.channel.name + '</a>'
+                );
                 if (index > 1) {
                     return false;
                 }
             });
-
-            // Add elements on the page
-            $('.navbar-header').find("ul").append( listTags.join('') );
-            $("li").each(function(index){
-                $( this ).append(images[index]);
-            });
         });
 
         $("nav").on("click", ".link", function() {
-            console.log($( this ));
+            //console.log($( this ));
         });
     });
 })();
